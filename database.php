c45397102bc3d161ead0f3f3ca1485b96155ebaf
shantigok123 <bustinjeiber1011@gmail.com>
2022-04-25 15:01:51 +0800

    Initial Commit

---
+++ database.php
@@ -0,0 +1,36 @@
+<?php 
+
+class DBconn{
+
+    private $host = "mysql:host=127.0.0.1;dbname=ipms";
+    private $username = "root";
+    private $password = "";
+    private $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => 
+    PDO::FETCH_ASSOC);
+
+    public $conn;
+
+    public function openCon()
+    {
+        try
+        {
+            $this->conn = new PDO($this->host, $this->username, $this->password, $this->options);
+            return $this->conn;
+            
+        }
+        catch(PDOException $e)
+        {
+            echo "Connection error: " . $e->getMessage();
+        }
+    }
+
+}
+
+
+function escStr($value) {
+    $escape_string = stripslashes(trim(($value)));
+    return $escape_string;
+}
+
+
+?>
